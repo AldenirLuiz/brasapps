ec88efcada444639205d6f59ca078e5aa923b69c
Aldenir Luiz <aldenir.sky@gmail.com>
2023-01-02 16:28:02 -0300

    adsCards aded

--- scripts/script.js
+++ scripts/script.js
@@ -8,32 +8,3 @@ function executaAcao(){
     window.location = "http://google.com";
 }
 
-function eventHeadfixed() {
-    var cabecalho = document.querySelector('.spaceBacground');
-    var cabecalhoEspaco = document.querySelector('.prymary');
-    var cabecalhoOffset = cabecalho.offsetTop;
-    
-    function verificaCabecalho() {
-        var scrollTop = window.scrollY;
-        var cabecalhoAltura = cabecalho.offsetHeight;
-
-        if(scrollTop >= cabecalhoOffset) {
-            // rolagem da página passou do elemento, vamos fixá-lo
-            cabecalho.classList.add('spaceBacgroundFixed');
-            
-            // ativa cabecalho-espaco para ocupar o espaço perdido
-            cabecalhoEspaco.style.height = cabecalhoAltura + 'px';
-        } else {
-            // retira a classe adicional do elemento
-            cabecalho.classList.remove('spaceBacgroundFixed');
-
-            // retira a altura do cabecalho-espaco
-            cabecalhoEspaco.style.height = 0;
-        }
-    }
-
-    window.addEventListener('scroll', verificaCabecalho);
-    window.addEventListener('resize', verificaCabecalho);
-}
-
-window.addEventListener("load", eventHeadfixed)
