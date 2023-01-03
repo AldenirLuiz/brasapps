8a52cb2b6d24203921ed2d619c9005e31e06d102
Aldenir Luiz <aldenir.sky@gmail.com>
2023-01-02 22:55:07 -0300

    footer aded, buttons functio aded

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,22 @@
 
-function bttFunction() {
-    alert("Nenhum Conteudo encontrado")
+function bttFunctionMore(param, section) {
+    let paragraph = document.getElementById("LivroEli");
+    let bttMore = document.getElementById("btnMore"+section);
+    let bttMin = document.getElementById("btnMin"+section);
+    if (param == "more") {
+        alert(param + " if stantment pass");
+        paragraph.classList.remove("hiddenText");
+        paragraph.classList.add("visibleText");
+        bttMore.classList.remove("visibleText");
+        bttMore.classList.add("hiddenText");
+        bttMin.classList.add("visibleText")
+    } else {
+        alert(param + " if stantment not pass");
+        paragraph.classList.remove("visibleText");
+        paragraph.classList.add("hiddenText");
+        bttMore.classList.add("visibleText");
+        bttMin.classList.remove("visibleText")
+    }
 }
 
 function executaAcao(){
