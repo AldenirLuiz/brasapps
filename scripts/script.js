50a078737af930f6286367a96ccbd07b71a750b4
Aldenir Luiz <aldenir.sky@gmail.com>
2023-01-15 12:26:32 -0300

    new page

--- scripts/script.js
+++ scripts/script.js
@@ -30,42 +30,60 @@ function bttFunctionMore(section) {
     }
 }
 
+/* Contador */
 
-
-/* Script Contato */
-
-const style = document.getElementById("primaryCss");
-const style1 = document.getElementById("secondaryCss");
-
-
-if (internet()) {
-    addBanerGithub();
-}else{
-    console.log("Internet Dissabled")
-}
-
-function addBanerGithub () {
-    let banner = document.getElementById("github-stats");
-    let codeBanner = `<p dir="auto"><a target="_blank" rel="noopener noreferrer nofollow" href="https://camo.githubusercontent.com/1aabfa667e145f74b3241fb5d9f2e3e8cd7d0314337574f15585ec396a2e0dd5/68747470733a2f2f6769746875622d726561646d652d73746174732e76657263656c2e6170702f6170693f757365726e616d653d416c64656e69724c75697a2673686f775f69636f6e733d74727565267468656d653d6461726b26636f756e745f707269766174653d74727565"><img src="https://camo.githubusercontent.com/1aabfa667e145f74b3241fb5d9f2e3e8cd7d0314337574f15585ec396a2e0dd5/68747470733a2f2f6769746875622d726561646d652d73746174732e76657263656c2e6170702f6170693f757365726e616d653d416c64656e69724c75697a2673686f775f69636f6e733d74727565267468656d653d6461726b26636f756e745f707269766174653d74727565" alt="Aldenir GitHub stats" data-canonical-src="https://github-readme-stats.vercel.app/api?username=AldenirLuiz&amp;show_icons=true&amp;theme=dark&amp;count_private=true" style="max-width: 100%;"></a></p>`;
-    banner.innerHTML = codeBanner;
-}
-
-let styleTypeLoader = window.setInterval(function () {
-    var widthDevice = screen.width;
-    if (widthDevice >= 640){
-        console.log(widthDevice)
-        style.setAttribute("href", "../css/style-contato.css");
-        style1.setAttribute("href", ""); 
+function ctck() {
+    var sds = document.getElementById("dum");
+    if(sds == null){
+        alert("Esta utilizando un paquete gratuito. No esta autorizado para retirar la etiqueta.");
     }
-    else{
-        console.log(widthDevice)
-        style1.setAttribute("href", "../css/style-contatoAlternative.css");
-        style.setAttribute("href", "");
+    var sdss = document.getElementById("dumdiv");
+    if(sdss == null){
+        alert("Esta utilizando un paquete gratuito. No esta autorizado para retirar la etiqueta.");
     }
-},1000)
-
-function internet () {
-    return navigator.onLine;
 }
 
-/* Fim Script Contato */
+document.onload="ctck()";
+function getCookieVal (offset) {
+    var endstr = document.cookie.indexOf (";", offset);
+    if (endstr == -1) {
+        endstr = document.cookie.length;
+        return unescape(document.cookie.substring(offset, endstr));
+    }
+}
+
+function GetCookie (name) {
+    var arg = name + "=";
+    var alen = arg.length;
+    var clen = document.cookie.length;
+    var i = 0;
+    while (i < clen) {
+        var j = i + alen;
+        if (document.cookie.substring(i, j) == arg) return getCookieVal (j);
+        i = document.cookie.indexOf(" ", i) + 1;
+        if (i == 0) break;
+    }
+    return null;
+}
+function SetCookie (name, value) {
+    var argv = SetCookie.arguments;
+    var argc = SetCookie.arguments.length;
+    var expires = (2 < argc) ? argv[2] : null;
+    var path = (3 < argc) ? argv[3] : null;
+    var domain = (4 < argc) ? argv[4] : null;
+    var secure = (5 < argc) ? argv[5] : false;
+    document.cookie = name + "=" + escape (value) + ((expires == null) ? "" : ("; expires=" + expires.toGMTString())) + ((path == null) ? "" : ("; path=" + path)) + ((domain == null) ? "" : ("; domain=" + domain)) + ((secure == true) ? "; secure" : "");
+}
+
+function DisplayInfo(){
+    var expdate = new Date();
+    var visit;
+    expdate.setTime(expdate.getTime()+(24 * 60 * 60 * 1000 * 365));
+    if(!(visit = GetCookie("visit"))) visit = 0;
+    visit ++ ;
+    SetCookie("visit", visit, expdate, "/", null, false);
+    document.write("Usted ha visitado esta pagina "+ visit + " veces"); 
+}
+
+document.onload=DisplayInfo();
+
