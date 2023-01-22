c8376b0a9573b8984b30e4273a28fa5f4284fa97
Aldenir Luiz <aldenir.sky@gmail.com>
2023-01-22 09:48:34 -0300

    link fixed

--- scripts/script.js
+++ scripts/script.js
@@ -5,100 +5,12 @@ let     :-> Declara uma variável local de escopo do bloco, opcionalmente, inici
 const   :-> Declara uma constante de escopo de bloco, apenas de leitura.
 */
 
-/* Esta função adiciona uma pagina dentro de um objeto com id: "idCard"
-function queryPageLoad(idCard, idClic) {
-    let target = document.getElementById(idCard);
-    var pageIteligencia = '../pages/contato.html'
-    let input = document.getElementById(idClic);
-    target.innerHTML = `<div class='card backgroundCard'><object id='flowObject' type='text/html' data='${pageIteligencia}'></object></div>`;
-}
-*/
-
-
-/*  Not Used
-function bttFunctionMore(section) {
-    let paragraph = document.getElementById(section);
-    let bttMin = document.getElementById("btnMin"+section);
-    if (bttMin.textContent == "Mais") {
-        alert("called plus")
-        bttMin.textContent = "Menos"
-        paragraph.classList.remove("hiddenText");
-        paragraph.classList.add("visibleText");
-    } else {
-        alert("called minus")
-        bttMin.textContent = "Mais"
-        paragraph.classList.remove("visibleText");
-        paragraph.classList.add("hiddenText");
-    }
-}
-*/
-
 
 function goToPage(page) {
-    let linked = window.location;
-    let orign = window.location.origin
-    if (page == "") {
-        window.location.replace(orign)
+    if (page == "home") {
+        window.location.replace(origin)
     }else{
-        window.location.replace(linked+page)
-    }
-    
-}
-
-/* Contador */
-
-function ctck() {
-    var sds = document.getElementById("dum");
-    if(sds == null){
-        alert("Esta utilizando un paquete gratuito. No esta autorizado para retirar la etiqueta.");
-    }
-    var sdss = document.getElementById("dumdiv");
-    if(sdss == null){
-        alert("Esta utilizando un paquete gratuito. No esta autorizado para retirar la etiqueta.");
+        window.location.replace(origin+page);
     }
 }
-
-document.onload="ctck()";
-function getCookieVal (offset) {
-    var endstr = document.cookie.indexOf (";", offset);
-    if (endstr == -1) {
-        endstr = document.cookie.length;
-        return unescape(document.cookie.substring(offset, endstr));
-    }
-}
-
-function GetCookie (name) {
-    var arg = name + "=";
-    var alen = arg.length;
-    var clen = document.cookie.length;
-    var i = 0;
-    while (i < clen) {
-        var j = i + alen;
-        if (document.cookie.substring(i, j) == arg) return getCookieVal (j);
-        i = document.cookie.indexOf(" ", i) + 1;
-        if (i == 0) break;
-    }
-    return null;
-}
-function SetCookie (name, value) {
-    var argv = SetCookie.arguments;
-    var argc = SetCookie.arguments.length;
-    var expires = (2 < argc) ? argv[2] : null;
-    var path = (3 < argc) ? argv[3] : null;
-    var domain = (4 < argc) ? argv[4] : null;
-    var secure = (5 < argc) ? argv[5] : false;
-    document.cookie = name + "=" + escape (value) + ((expires == null) ? "" : ("; expires=" + expires.toGMTString())) + ((path == null) ? "" : ("; path=" + path)) + ((domain == null) ? "" : ("; domain=" + domain)) + ((secure == true) ? "; secure" : "");
-}
-
-function DisplayInfo(){
-    var expdate = new Date();
-    var visit;
-    expdate.setTime(expdate.getTime()+(24 * 60 * 60 * 1000 * 365));
-    if(!(visit = GetCookie("visit"))) visit = 0;
-    visit ++ ;
-    SetCookie("visit", visit, expdate, "/", null, false);
-    document.write("Usted ha visitado esta pagina "+ visit + " veces"); 
-}
-
-document.onload=DisplayInfo();
 
